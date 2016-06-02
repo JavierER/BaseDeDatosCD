7d7915ddf5c39d879c80022eb1ba936336540f3a
Javii <Javii@Javi>
2016-06-02 09:32:57 +0200

    conection y resultset inicializados a null

---
+++ src/LibreriaCD/Metodos.java
@@ -0,0 +1,15 @@
+package LibreriaCD;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+
+
+/**
+ *
+ * @author Javii
+ */
+public class Metodos {
+    
+    private Connection conexion = null;
+    private ResultSet rs = null;
+}
